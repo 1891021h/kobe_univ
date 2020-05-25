e0a2cb4c5ba2ded7818910e3d22c4703c4d9d5c5
1891021h <65202866+1891021h@users.noreply.github.com>
2020-05-25 16:53:08 +0900

    mecha commit
    解説ページのアップ

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-document.getElementById("baz").addEventListener("click",function(){
-    alert('だめっていったでしょ'); 
-    alert('いいこにしなさい'); 
-});
+/* document.getElementById("baz").addEventListener("click", function() {
+     alert("ボタンが有効です");
+ })
+
+ const button = document.getElementById("baz");
+ const lists = document.getElementById("lists")
+ button.addEventListener("click", function(){
+
+     const list = document.createElement("li");
+     list.innertext = "test";
+     lists.appendChild(list);
+     console.log(list);
+      document.write(list);
+ }) */
+
+ document.write ("JapaScriptの位置");
+
+ /* const myHeading = document. querySelector("h1");
+ myHeading.textContent = 'りょうちゃだよ'; */
+
